e560fc463dc386faa87a28bde4a44b8159848ff3
janet78 <jeannebonnel@bonnel5.rez-gif.supelec.fr>
2016-03-23 09:37:22 +0100

    mercredi 09:37

--- src/Jeu/Empilement.java
+++ src/Jeu/Empilement.java
@@ -1,42 +1,29 @@
 package Jeu;
 import java.util.*;
-
-<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.*;
-import Main.Brique;
+
 
 
 public class Empilement {
 	public String combinaisonCouleurs[];
-	
 	public Partie partie;
-	public Brique[] pileBriques;
-	
-	ArrayList <Brique> empilement =new ArrayList <Brique>();
-	
-	//faire code pour afficher empilement au dessus de la case
-
-import javax.swing.JLabel;
-
-@SuppressWarnings("serial")
-public class Empilement extends JLabel{
-
 	public ArrayList <Brique> pileBriques;
+	
 	
 	public Empilement(){
 		this.pileBriques = new ArrayList<Brique>();
 	}
-		
+	
+	public ArrayList <Brique> getPileBriques(){
+		return pileBriques;
+	}
 	
 	public void ajouterBrique(Brique brique){
 		this.pileBriques.add(brique);
 	}
 	 
-	public ArrayList <Brique> getPileBriques(){
-		return this.pileBriques;
-	 }
-	
+	 
 	
 	//crŽation de la combinaison de couleur 
 	public String [] getCombinaisonCouleurs(){
@@ -47,6 +34,6 @@ public class Empilement extends JLabel{
 	return listeCombinaisonCouleurs;
 		
 	}
->>>>>>> origin/master
+
 	
 }
